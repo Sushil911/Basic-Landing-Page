6010f898962d46baf308fe780adab094a28a12e4
Sushil911 <Sushilbasyal30@gmail.com>
2024-09-21 15:32:46 +0545

    Made responsive navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import Header from './sections/Header';
+import Hero from './sections/Hero';
 
 const App = () => {
   return (
-    <div>App</div>
+    <main className='overflow-hidden'>
+       <Header />
+       <Hero />
+    </main>
   )
 }
 
